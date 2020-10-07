edbfe6d63d6607029f8fed58f17d249710042b11
Isaac Friedman <isaac@isaac-friedman.com>
2020-10-07 10:36:11 -0400

    Viewports

--- cypress/page-objects/BasePage.js
+++ cypress/page-objects/BasePage.js
@@ -1,9 +1,23 @@
 export default class BasePage {
+  //This would be a good candidate for our custom command
   static waitS(seconds) {
     cy.wait(seconds * 1000);
   }
 
   static logMsg(message) {
-    cy.log(message)
+    cy.log(message);
+  }
+
+  static mobileScreen() {
+    //use our custom commands
+    cy.setViewport('samsung-s9');
+  }
+
+  static tabletScreen() {
+    cy.setViewport('ipad-2')
+  }
+
+  static laptopScreen() {
+    cy.setViewport('macbook-15')
   }
 }
