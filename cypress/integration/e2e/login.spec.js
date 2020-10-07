5cc04b7e418c5fd67cbc857096d2ec9d530f1f5d
Isaac Friedman <isaac@isaac-friedman.com>
2020-10-07 15:30:09 -0400

    IT WERKS!!!!1!

--- cypress/integration/e2e/login.spec.js
+++ cypress/integration/e2e/login.spec.js
@@ -1,7 +1,24 @@
+import { url } from '../../../config'
+import TopNav from '../../page-objects/components/topNav'
+import LoginPage from '../../page-objects/pages/loginPage'
+
 describe("successful login", () => {
-  //TODO
+  before(() => {
+    cy.visit(url)
+    TopNav.clickSignin()
+  })
+  it("Should log in", () => {
+    LoginPage.login("username", "password") //identical to cy.login. Miserable architecture
+  })
 })
 
 describe("unsuccessful login", () => {
-  //TODO
+  before(() => {
+    cy.visit(url)
+    TopNav.clickSignin()
+  })
+  it("Shouldn't log in", () => {
+    cy.login('badname', 'badpass')
+    LoginPage.displayBadPassErr()
+  })
 })
