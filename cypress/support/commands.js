e597c6cdabf7a58939681e4f4dcff444d153838b
Isaac Friedman <isaac@isaac-friedman.com>
2020-10-07 10:03:52 -0400

    Is-hidden command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -30,3 +30,7 @@ addMatchImageSnapshotCommand();
 Cypress.Commands.add('isVisible', selector => {
   cy.get(selector).should('be.visible')
 })
+
+Cypress.Commands.add('isHidden', selector => {
+  cy.get(selector).should('not.be.visible')
+})
