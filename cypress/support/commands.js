9ba840eb55e6a4667eeb73a13478c528605791be
Isaac Friedman <isaac@isaac-friedman.com>
2020-10-07 10:15:11 -0400

    Login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -43,6 +43,14 @@ Cypress.Commands.add('setViewport', size => {
   if(Array.isArray(size)){
     cy.viewport(size[0], size[1])
   } else {
-    cy.viewport(size) 
+    cy.viewport(size)
   }
+})
+
+Cypress.Commands.add('login', (username, password) => {
+  cy.get('#login_form').should('be.visible');
+  cy.get('#user_login').type(username);
+  cy.get('#user_password').type(password);
+  cy.get('#user_remember_me').click();
+  cy.contains('Sign in').click()
 })
