9648d42decc18be951c22d67fc00ec4143713c90
Isaac Friedman <isaac@isaac-friedman.com>
2020-10-07 13:20:10 -0400

    method stubs for login page

---
+++ cypress/page-objects/pages/loginPage.js
@@ -0,0 +1,13 @@
+export default class LoginPage {
+  static login(username, password) {
+    //TODO
+  }
+
+  static forgotPass() {
+    //TODO
+  }
+
+  static displayBadPassErr() {
+    //TODO
+  }
+}
